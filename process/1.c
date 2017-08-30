4b5edd3dec905c7b098ac03d651a649d70935ddd
zhangh <2162990048@qq.com>
2017-08-30 17:07:00 +0800

    add fd

--- process/1.c
+++ process/1.c
@@ -2,14 +2,19 @@
 #include <unistd.h>
 void main(){
     int i;
-    if (fork() == 0) {
+    int fd =0;
+    fd = fork();
+    printf("fd = %d \n",fd);
+    if (fd== 0) 
+    {
         /* 子进程程序 */
-        for (i = 1; i <100; i++)
-            printf("This is child process\n");
+        for (i = 1; i <10; i++)
+            printf(" %d This is child process 子进程程序 \n",i);
     }
-    else {
+    else 
+    {
         /* 父进程程序*/
-        for (i = 1; i <100; i++)
-            printf("This is parent process\n");
+        for (i = 1; i <10; i++)
+            printf("%d This is parent process 父进程程序 \n",i);
     }
 }
