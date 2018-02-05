4c5dd913a4efe8fe0454f004ee4bec008a0b9bba
zhangh <2162990048@qq.com>
2018-02-05 11:57:21 +0800

    add unix

---
+++ unix/3-11.c
@@ -0,0 +1,36 @@
+#include "apue.h"
+#include <fcntl.h>
+int main(int argc, char *argv[])
+{
+	int val;
+	if(argc !=2)
+		err_quit("usage: a.out <descroptor #>");
+	if((val = fcntl(atoi(argv[1]),F_GETFL,0))<0)
+		err_sys("fcnt error for fd %d",atoi(argv[1]));
+	switch(val & O_ACCMODE)
+	{
+		case O_RDONLY:
+			printf("read only");
+			break;
+		case O_WRONLY:
+			printf("write only");
+			break;
+		default:
+			err_dump("unknown access mode");
+	}
+	if(val & O_APPEND)
+		printf(",append");
+	if(val & O_NONBLOCK)
+		printf(",noblocking");
+	if(val & O_SYNC)
+		printf(",synchronous writes");	
+
+	#if  !defined(_POSIX_C_SOURCE) && defined(O_FSYNC) && (O_FSYNC != O_SYNC)
+	if(val & O_FSYNC)
+		printf(",synchronous writes");
+	#endif
+	putchar('\n');
+	exit(0);
+
+}
+
